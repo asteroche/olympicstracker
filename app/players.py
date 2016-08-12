cadab7bbb1814b1a3ad73c1eb454f05420533f5f
Tyroo <tyler.roche@gmail.com>
2016-08-11 23:12:59 -0700

    Test: adding incremental scores
    Adding incremental score counts in more details to table

--- app/players.py
+++ app/players.py
@@ -3,14 +3,14 @@
 import medalParser
 
 players = [
-		{'name': 'Tyler', 'Golds':'Brazil', 'Silvers':'Kazakhstan', 'Bronzes':'Colombia', 'Score':0}, 
-		{'name': 'Anna', 'Golds':'Spain', 'Silvers':'Ethiopia', 'Bronzes':'Czech Republic', 'Score':0},
-		{'name': 'Nelson', 'Golds':'North Korea', 'Silvers':'Poland', 'Bronzes':'Canada', 'Score':0},
-		{'name': 'Daniel', 'Golds':'Netherlands', 'Silvers':'Azerbaijan', 'Bronzes':'Romania', 'Score':0},
-		{'name': 'Nick', 'Golds':'Ukraine', 'Silvers':'Belarus', 'Bronzes':'Danmark', 'Score':0}, 
-		{'name': 'Matt', 'Golds':'New Zealand', 'Silvers':'Cuba', 'Bronzes':'Mexico', 'Score':0},
-		{'name': 'Jess', 'Golds':'Hungary', 'Silvers':'Jamaica', 'Bronzes':'Sweden', 'Score':0},
-		{'name': 'James', 'Golds':'Iran', 'Silvers':'Kenya', 'Bronzes':'Ireland', 'Score':0}]
+		{'name': 'Tyler', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Brazil', 'Silvers':'Kazakhstan', 'Bronzes':'Colombia', 'Score':0}, 
+		{'name': 'Anna', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Spain', 'Silvers':'Ethiopia', 'Bronzes':'Czech Republic', 'Score':0},
+		{'name': 'Nelson', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'North Korea', 'Silvers':'Poland', 'Bronzes':'Canada', 'Score':0},
+		{'name': 'Daniel', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Netherlands', 'Silvers':'Azerbaijan', 'Bronzes':'Romania', 'Score':0},
+		{'name': 'Nick', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Ukraine', 'Silvers':'Belarus', 'Bronzes':'Danmark', 'Score':0}, 
+		{'name': 'Matt', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'New Zealand', 'Silvers':'Cuba', 'Bronzes':'Mexico', 'Score':0},
+		{'name': 'Jess', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Hungary', 'Silvers':'Jamaica', 'Bronzes':'Sweden', 'Score':0},
+		{'name': 'James', 'Goldscore':0, 'Silverscore':0, 'Bronzescore':0, 'Golds':'Iran', 'Silvers':'Kenya', 'Bronzes':'Ireland', 'Score':0}]
 		#defines the teams that players picked
 
 def getPlayers():
@@ -26,7 +26,10 @@ def updateScore():
 		defaultValue = {'Golds': 0, 'Silvers': 0, 'Bronzes': 0}
 
 		goldScore = int(countryData.get(player['Golds'], defaultValue)['Golds'])
+		player['Goldscore']=goldScore
 		silverScore = int(countryData.get(player['Silvers'], defaultValue)['Silvers'])
+		player['Silverscore']=silverScore
 		bronzeScore = int(countryData.get(player['Bronzes'], defaultValue)['Bronzes'])
+		player['Bronzescore']=bronzeScore
 ##have kyle explain this later
 		player['Score'] = goldScore + silverScore + bronzeScore
